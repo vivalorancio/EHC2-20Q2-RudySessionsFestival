b8eb75b56a9d9da07357bc24d98dfdbb206a0bad
vivalorancio <vmateu@gmail.com>
2020-12-04 20:47:55 +0100

    contact

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,6 +6,7 @@
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 import 'bootstrap';
+import 'leaflet';
 
 /**
  * Write any other JavaScript below
@@ -14,3 +15,9 @@ import 'bootstrap';
 // +(function () {
 //   console.log('Hello, UOC!');
 // })();
+
+let map = L.map('map').setView([39.5794644, 2.6371057], 18);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
