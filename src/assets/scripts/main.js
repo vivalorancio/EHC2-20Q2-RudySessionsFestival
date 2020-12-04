31932b0587f94cbc6e6c2f9ff3011a046f105bf6
vivalorancio <vmateu@gmail.com>
2020-12-04 19:58:01 +0100

    first commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,16 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+import 'bootstrap';
+
+/**
+ * Write any other JavaScript below
+ */
+
+// +(function () {
+//   console.log('Hello, UOC!');
+// })();
